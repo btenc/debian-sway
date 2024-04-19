2ba7c88c2edb1693e5042859216f35225e3b921d
William Ten Cate <billytencate@gmail.com>
2024-04-18 21:51:57 -0400

    Fix log output

--- install.sh
+++ install.sh
@@ -16,6 +16,7 @@ fi
 
 username=$(id -u -n 1000)
 user_home="/home/$username"
+echo "apt-log will be located at /home/$username"
 
 # Home directories to be created
 directories=(.config Pictures/wallpapers Documents Videos Music appimages deb Downloads dev/repos dev/scripts disks)
@@ -41,61 +42,61 @@ chown -R $username:$username "$user_home" || echo "Failed to change ownership to
 
 # System update:
 echo "Updating system packages..."
-apt update &> ~/apt-log.txt && apt upgrade -y &> ~/apt-log.txt
+apt update &> "${user_home}/apt-log.txt" && apt upgrade -y &> "${user_home}/apt-log.txt"
 
 # Installation of various packages
 echo "Installing base system utilities..."
-apt install -y build-essential eject zip unzip parted wget whois lshw apt-transport-https dirmngr curl ssh traceroute iw acl ufw acpi tree gpg debian-archive-keyring udns-utils &> ~/apt-log.txt
+apt install -y build-essential eject zip unzip parted wget whois lshw apt-transport-https dirmngr curl ssh traceroute iw acl ufw acpi tree gpg debian-archive-keyring udns-utils &> "${user_home}/apt-log.txt"
 
 echo "Installing additional utilities..."
-apt install -y lxpolkit xfce4-power-manager arandr network-manager &> ~/apt-log.txt
+apt install -y lxpolkit xfce4-power-manager arandr network-manager &> "${user_home}/apt-log.txt"
 
 echo "Installing Xorg display server..."
-apt install -y xserver-xorg x11-xserver-utils x11-utils xinit dbus-x11 &> ~/apt-log.txt
+apt install -y xserver-xorg x11-xserver-utils x11-utils xinit dbus-x11 &> "${user_home}/apt-log.txt"
 
 echo "Installing audio management packages..."
-apt install -y pipewire wireplumber pavucontrol &> ~/apt-log.txt
-sudo -u $username systemctl --user enable wireplumber.service || echo "Failed to enable wireplumber service for $username"
+apt install -y pipewire wireplumber pavucontrol &> "${user_home}/apt-log.txt"
+systemctl --user enable wireplumber.service || echo "Failed to enable wireplumber service for $username"
 
 echo "Installing preferred applications..."
-apt install -y vim zoxide ranger htop neofetch figlet qalc &> ~/apt-log.txt
-apt install -y vlc feh audacity gparted gimp flameshot strawberry libreoffice thunderbird qalculate-gtk imagemagick &> ~/apt-log.txt
+apt install -y vim zoxide ranger htop neofetch figlet qalc &> "${user_home}/apt-log.txt"
+apt install -y vlc feh audacity gparted gimp flameshot strawberry libreoffice thunderbird qalculate-gtk imagemagick &> "${user_home}/apt-log.txt"
 
 echo "Installing file manager and related plugins..."
-apt install -y thunar thunar-archive-plugin thunar-gtkhash thunar-media-tags-plugin thunar-volman &> ~/apt-log.txt
+apt install -y thunar thunar-archive-plugin thunar-gtkhash thunar-media-tags-plugin thunar-volman &> "${user_home}/apt-log.txt"
 
 echo "Installing terminal..."
-apt install -y kitty &> ~/apt-log.txt
+apt install -y kitty &> "${user_home}/apt-log.txt"
 
 echo "Installing browser and PWA functionality..."
-apt install -y firefox-esr &> ~/apt-log.txt
+apt install -y firefox-esr &> "${user_home}/apt-log.txt"
 if curl -fsSL https://packagecloud.io/filips/FirefoxPWA/gpgkey | gpg --dearmor | sudo tee /usr/share/keyrings/firefoxpwa-keyring.gpg > /dev/null; then
   echo "deb [signed-by=/usr/share/keyrings/firefoxpwa-keyring.gpg] https://packagecloud.io/filips/FirefoxPWA/any any main" | sudo tee /etc/apt/sources.list.d/firefoxpwa.list > /dev/null
-  apt update &> ~/apt-log.txt && apt install -y firefoxpwa &> ~/apt-log.txt || { echo "Firefox PWA installation failed.";}
+  apt update &> "${user_home}/apt-log.txt" && apt install -y firefoxpwa &> "${user_home}/apt-log.txt" || { echo "Firefox PWA installation failed.";}
 else
   echo "Failed to download or process the GPG key for Firefox PWA."
 fi
 
 echo "Installing fonts..."
-apt install -y fonts-recommended fonts-font-awesome &> ~/apt-log.txt
-fc-cache -vf &> ~/apt-log.txt
+apt install -y fonts-recommended fonts-font-awesome &> "${user_home}/apt-log.txt"
+fc-cache -vf &> "${user_home}/apt-log.txt"
 
 echo "Setting up display manager..."
-apt install -y lightdm light-locker &> ~/apt-log.txt
-systemctl enable lightdm &> ~/apt-log.txt
-systemctl set-default graphical.target &> ~/apt-log.txt
+apt install -y lightdm light-locker &> "${user_home}/apt-log.txt"
+systemctl enable lightdm || echo "Failed to enable display manager"
+systemctl set-default graphical.target || echo "Failed to set default graphical target"
 
 echo "Installing window management tools..."
-apt install -y openbox obconf tint2 menu rofi picom dunst lxappearance &> ~/apt-log.txt
+apt install -y openbox obconf tint2 menu rofi picom dunst lxappearance &> "${user_home}/apt-log.txt"
 
 echo "Installing local .deb packages..."
 deb_dir="$user_home/deb"
 if compgen -G "${deb_dir}/*.deb" > /dev/null; then
-  apt install -y "$deb_dir"/*.deb &> ~/apt-log.txt
+  apt install -y "$deb_dir"/*.deb &> "${user_home}/apt-log.txt"
 else
   echo "No .deb files found in $deb_dir to install."
 fi
 
-apt autopurge &> ~/apt-log.txt
+apt autopurge &> "${user_home}/apt-log.txt"
 
 echo "Setup complete. You can now reboot."
